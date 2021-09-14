4426f4915c633fee38cd2de1301f0dbf6a0f5eab
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-09-14 11:14:11 -0500

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -399,7 +399,7 @@ body = html.Div([
                     " relevancia en la agenda seguridad pública del país, también."
                     " Entre 2015 y 2021 se registraron "+ str(f"{int(TOTVSLIBPERSONAL):,}") +" casos, lo que representa una tasa de "+
        str(TASAVSLIBPERSONAL) +" delitos por cada 100 mil habitantes. "+
-      "En este dashboard analítico observamos su gravedad según intervalos anuales e intervalos mensuales"+
+      "En este tablero analítico observamos su gravedad según intervalos anuales e intervalos mensuales"+
       "; también incluimos un análisis detallado de las cuatro entidades con más incidencias en este delito"+
       "; finalmente, comparamos los rankings por entidad según sumas acumuladas respecto a las tasas, ambas "+
       "por entidad y ambas del periódo 2015 al 2021.",
@@ -706,14 +706,14 @@ body = html.Div([
                 html.Br(),
                 html.H4("Metodología "),
                 html.P(
-                    "El presente dashboard es un ejercicio institucional con el objeto de "
+                    "El presente tablero es un ejercicio institucional con el objeto de "
                     "informar a las diputadas y diputados y público interesado sobre un tema "
                     "de vital importancia en la vida política. "
                     "La metodología que hemos empleado para analizar los datos la detallamos enseguida. "
                     "Como se indica en cada caso, la información sobre el delito aborto "
                     "fue obtenida del Secretariado Ejecutivo Nacional del Sistema Nacional de Seguridad Pública (SENSNSP) (2015-2021); "
                     " "
-                    "Este dashboard seguramente será completado progresivamente con otras fuentes de información "
+                    "Este tablero seguramente será completado progresivamente con otras fuentes de información "
                     "tanto gubernamental, como aquella proveniente de organizaciones civiles que " 
                     "dan seguimiento al tema. "
                     "En ningún caso, este contenido representa algún "
@@ -781,5 +781,3 @@ app.layout = html.Div([body],
 if __name__ == "__main__":
     app.run_server()
 
-
- 
