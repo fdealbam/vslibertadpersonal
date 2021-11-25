d2a1f293a642e5e561807ab7cc78d1994505e783
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-11-24 22:15:06 -0600

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -120,7 +120,7 @@ femi15_21 = ff[[
  'Agosto20','Septiembre20','Octubre20','Noviembre20','Diciembre20',
     
  'Enero21','Febrero21','Marzo21','Abril21','Mayo21','Junio21','Julio21',
- 'Agosto21','Septiembre21',#'Octubre21','Noviembre21','Diciembre21'
+ 'Agosto21','Septiembre21','Octubre21',#'Noviembre21',#'Diciembre21'
              ]]
 
 
@@ -147,7 +147,7 @@ femi15_21['Total2020']= femi15_21[[ 'Enero20', 'Febrero20', 'Marzo20', 'Abril20'
 
 femi15_21['Total2021']= femi15_21[[ 'Enero21','Febrero21', 'Marzo21', 'Abril21', 'Mayo21',
                                    'Junio21','Julio21','Agosto21',
-                                   'Septiembre21',#'Octubre21',
+                                   'Septiembre21','Octubre21',
                                    #'Noviembre21','Diciembre21'
                                   ]].sum(axis=1)
 
@@ -183,7 +183,7 @@ pagra = ff[[
     'Septiembre20','Octubre20', 'Noviembre20', 'Diciembre20',
 
  'Enero21', 'Febrero21', 'Marzo21','Abril21', 'Mayo21', 'Junio21', 'Julio21', 'Agosto21',
-    'Septiembre21',#'Octubre21','Noviembre21','Diciembre21'
+    'Septiembre21','Octubre21',#'Noviembre21',#'Diciembre21'
             ]]
 
 
@@ -533,7 +533,7 @@ body = html.Div([
                        dbc.Badge("mensuales", color="info", className="mr-1")]), 
                                        width={'size': 11,  "offset":1 })]),
        dbc.Row([        
-               dbc.Col(html.H5("(hasta septiembre 2021)"),
+               dbc.Col(html.H5("(hasta octubre 2021)"),
                                        width={ 'size': 3, "offset":1 }),
 
             ]),
